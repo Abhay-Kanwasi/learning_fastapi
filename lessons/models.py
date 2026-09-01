b1d98a29737558f8715bfd7e37aebc94b2533e06
Abhay Kanwasi <abhaykanwasi123@gmail.com>
2026-09-01 17:36:00 +0000

    Add delete user endpoint and update User model relationship for cascading deletes

--- lessons/models.py
+++ lessons/models.py
@@ -20,7 +20,7 @@ class User(Base):
         default=None,
     )
 
-    posts: Mapped[list[Post]] = relationship(back_populates="author")
+    posts: Mapped[list[Post]] = relationship(back_populates="author", cascade="all, delete-orphan") # It tell user if user is deleted delete user posts too
 
     @property
     def image_path(self) -> str:
